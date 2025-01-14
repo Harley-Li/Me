de2ec6400332ce1bf0af9a1388b835cd4914d4fb
Harley <harley.horizon01@gmail.com>
2025-01-14 22:06:05 +0800

    Add Navigator

---
+++ risk.ts
@@ -0,0 +1,31 @@
+const risks = {
+    riskLevel: [
+        {
+            title: 'Cautious',
+            desc: 'Low-risk investments typically produce more stable but modest returns.',
+            level: 1,
+        },
+        {
+            title: 'Moderately cautious',
+            desc: 'Low-risk investments typically produce more stable but modest returns.',
+            level: 2,
+        },
+        {
+            title: 'Moderately',
+            desc: 'Low-risk investments typically produce more stable but modest returns.',
+            level: 3,
+        },
+        {
+            title: 'Moderate bold',
+            desc: 'Low-risk investments typically produce more stable but modest returns.',
+            level: 4,
+        },
+        {
+            title: 'Bold',
+            desc: 'Low-risk investments typically produce more stable but modest returns.',
+            level: 5,
+        },
+    ],
+};
+
+export default risks;
